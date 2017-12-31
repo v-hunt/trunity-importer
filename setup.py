6e8593a748e6edf21bdb205afcb3edcb0dcec84d
VicHunting <VicHunting@yandex.ua>
2017-12-31 02:09:22 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trunity_importer',
-    version='0.2',
+    version='0.2.1',
     packages=find_packages(),
     scripts=['trunity_importer/bin/trunity-importer'],
     install_requires=[
