fbdf5160424238bb35a969e7d4e7f2798a713007
VicHunting <VicHunting@yandex.ua>
2018-01-10 02:16:36 +0200

    Extract some variables to base class in question containers

--- trunity_importer/sda/question_containers.py
+++ trunity_importer/sda/question_containers.py
@@ -9,7 +9,16 @@ class QuestionType:
 
 
 class Question:
-    pass
+
+    def __init__(self, type_: str, text: str,  audio_file: str,
+                 test_id: str, item_position: int, item_id: int):
+        self.text = text
+        self.audio_file = audio_file
+        self.test_id = test_id
+        self.item_position = item_position
+        self.item_id = item_id  # question id in xml
+
+        self.type = type_
 
 
 class MultipleChoice(Question):
@@ -19,15 +28,17 @@ class MultipleChoice(Question):
 
     def __init__(self, text: str, answers: List[Answer],
                  audio_file: str, test_id: str, item_position: int, item_id: int):
-        self.text = text
+
+        super(MultipleChoice, self).__init__(
+            type_=QuestionType.MULTIPLE_CHOICE,
+            text=text,
+            audio_file=audio_file,
+            test_id=test_id,
+            item_position=item_position,
+            item_id=item_id,
+        )
+
         self.answers = answers
-
-        self.audio_file = audio_file
-        self.test_id = test_id
-        self.item_position = item_position
-        self.item_id = item_id  # question id in xml
-
-        self.type = QuestionType.MULTIPLE_CHOICE
 
 
 class Essay(Question):
@@ -38,12 +49,15 @@ class Essay(Question):
     def __init__(self, text: str, correct_answer: str,
                  audio_file: str, test_id: str, item_position: int,
                  item_id: int):
-        self.text = text
+
+        super(Essay, self).__init__(
+            type_=QuestionType.ESSAY,
+            text=text,
+            audio_file=audio_file,
+            test_id=test_id,
+            item_position=item_position,
+            item_id=item_id,
+        )
+
         self.correct_answer = correct_answer
 
-        self.audio_file = audio_file
-        self.test_id = test_id
-        self.item_position = item_position
-        self.item_id = item_id  # question id in xml
-
-        self.type = QuestionType.ESSAY
