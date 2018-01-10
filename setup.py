5206425a224c153955a72125fc4f1ee6365e362d
VicHunting <VicHunting@yandex.ua>
2018-01-10 03:12:58 +0200

    Change version and include templates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trunity_importer',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(),
     scripts=['trunity_importer/bin/trunity-importer'],
     install_requires=[
@@ -11,12 +11,10 @@ setup(
         'beautifulsoup4',
         'trunity-3-client>=0.6.1',
       ],
-    url='',
+    url='https://github.com/v-hunt/trunity-importer',
     license='MIT',
     author='hunting',
     author_email='VicHunting@yandex.ua',
     description='Library for importing content from various sources to Trunity 3 LMS.',
-    data_files=[
-        ('trunity_importer/sda/templates', ['trunity_importer/sda/templates/question.html']),
-    ],
+    include_package_data=True,
 )
