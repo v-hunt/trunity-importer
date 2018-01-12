359366e6d66bb6448f8eb36747a577aa1b7cf368
VicHunting <VicHunting@yandex.ua>
2018-01-12 17:28:20 +0200

    Edit setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='trunity_importer',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     scripts=['trunity_importer/bin/trunity-importer'],
     install_requires=[
         'lxml',
         'beautifulsoup4',
-        'trunity-3-client>=0.6.1',
+        'trunity-3-client<0.7',
       ],
     url='https://github.com/v-hunt/trunity-importer',
     license='MIT',
